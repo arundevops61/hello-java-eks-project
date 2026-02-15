47d8f5e275d0921918e3dc02eb70321c38347788
arundevops61 <arundevops61@gmail.com>
2026-02-15 17:22:08 +0530

    eks-3

---
+++ app/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        port(8080);
+
+        get("/", (req, res) -> "Hello World from AWS EKS 🚀");
+
+    }
+}
